2f9d5accc0788922c22f333df51c0fe9c703ed13
SeanOne <basement365@gmail.com>
2019-03-03 21:58:56 -0800

    computation stretch truth table

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -10,6 +10,33 @@ char **reconstruct_trip(Ticket **tickets, int length)
   char **route = malloc(length * sizeof(char *));
 
   // YOUR CODE HERE
+  // create a counter for routes
+  int route_count = 0;
+  // create starting point
+  char *start_source = "NONE";
+
+  // iterate through tickets
+  for (int i = 0: i < tickets->capacity; i++) {
+    // create current pair
+    LinkedPair *current_ticket = create_pair(tickets[i]->source, tickets[i]->destination);
+
+    // compare start with current start
+    if (strcmp(current_ticket->key, start_source) != 0) {
+      // hash the current key
+      int hashed = hash(current_ticket->key, ht->capacity);
+      // check the ht for the current
+      int hash_check = hash_table_retrieve(ht, current_ticket->key);
+      if (hash_check < 0) {
+        hash_table_insert(ht, current_ticket->key, current_ticket->value);
+      } else { // if it is in the ht grab it and update start
+        
+
+      }
+
+
+    }
+  }
+  
 
   return route;
 }
